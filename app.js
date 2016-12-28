43e774a5fb54b516be3bd2151531f028deae8194
Meng Wang <water.wang@live.com>
2016-12-28 11:20:18 +0800

    Click to View

--- app.js
+++ app.js
@@ -4,3 +4,98 @@ var appmain = new Vue({
         message: 'Hello Vue!'
     }
 })
+var appcontrolpanel = new Vue({
+    el: '#appcontrolpanel',
+    data: {},
+    methods: {
+        loadmap: function(items) {
+            $.ajax({
+                url: 'bfgen.php',
+                type: 'GET',
+                data: {
+                    'mode': 1,
+                    'size': 20
+                },
+                success: function(data) {
+                    console.log(data);
+                    data = JSON.parse(data);
+                    appbfgen.length = [];
+                    for (i = 0; i < data.length; i++) {
+                        appbfgen.length.push(i);
+                    }
+                    appbfgen.battlefield = data;
+                },
+                error: function() {
+                    console.log("ERROR with failed REQUEST");
+                }
+            });
+        },
+        genmap: function() {
+            $.ajax({
+                url: 'bfgen.php',
+                type: 'GET',
+                data: {
+                    'mode': 0,
+                    'size': 24
+                },
+                success: function(data) {
+                    data = JSON.parse(data);
+                    appbfgen.length = [];
+                    for (i = 0; i < data.length; i++) {
+                        appbfgen.length.push(i);
+                    }
+                    appbfgen.battlefield = data;
+                },
+                error: function() {
+                    console.log("ERROR with failed REQUEST");
+                }
+            });
+        }
+    }
+})
+var appdetails = new Vue({
+    el: '#appdetails',
+    data: {
+        display: 'Ready',
+        seleted: false
+    },
+    methods: {
+
+    }
+})
+var appbfgen = new Vue({
+    el: '#appbfgen',
+    data: {
+        battlefield: [
+            [
+                ['test', 'y-meng'],
+                ['test', 'n-meng']
+            ],
+            [
+                ['test', 'y-meng'],
+                ['test', 'n-meng']
+            ]
+        ],
+        length: [0, 1]
+    },
+    methods: {
+        mousein: function(x, y) {
+            appdetails.seleted = true;
+            appdetails.display = 'Coordinate: ' + x + ':' + y;
+        },
+        mouseout: function(x, y) {
+            if (appdetails.seleted) {
+                appdetails.seleted = false;
+                appdetails.display = 'Ready';
+            }
+        },
+        clicked: function(x, y) {
+            appdetails.seleted = false;
+            appdetails.display = 'clicked: ' + x + ':' + y;
+        },
+        greet: function() {
+            console.log("GOODJOB");
+        }
+
+    }
+})
--- bfgen.php
+++ bfgen.php
@@ -13,10 +13,30 @@ switch ($Gmode) {
       }
   }
   for ($x = 0;$x<$Gsize-1;$x++) {
-      array_push($re[$x][0], $x, 'y-meng', 0);
-      array_push($re[0][$x+1], $x, 'y-meng', 0);
-      array_push($re[$Gsize-1][$x], $x, 'y-meng', 0);
-      array_push($re[$x+1][$Gsize-1], $x, 'y-meng', 0);
+      $ran = mt_rand(0, 6);
+      if ($ran != 1) {
+          array_push($re[$x][0], '', 'y-meng', 0, $x, 0);
+      } else {
+          array_push($re[$x][0], $ran, 'n-meng', 1, $x, 0);
+      }
+      $ran = mt_rand(0, 6);
+      if ($ran != 1) {
+          array_push($re[0][$x+1], '', 'y-meng', 0, 0, $x+1);
+      } else {
+          array_push($re[0][$x+1], $ran, 'n-meng', 1, 0, $x+1);
+      }
+      $ran = mt_rand(0, 6);
+      if ($ran != 1) {
+          array_push($re[$Gsize-1][$x], '', 'y-meng', 0, $Gsize-1, $x);
+      } else {
+          array_push($re[$Gsize-1][$x], $ran, 'n-meng', 1, $Gsize-1, $x);
+      }
+      $ran = mt_rand(0, 6);
+      if ($ran != 1) {
+          array_push($re[$x+1][$Gsize-1], '', 'y-meng', 0, $x+1, $Gsize-1);
+      } else {
+          array_push($re[$x+1][$Gsize-1], $ran, 'n-meng', 1, $x+1, $Gsize-1);
+      }
   }
   for ($x =1;$x<$Gsize-1;$x++) {
       for ($y =1;$y<$Gsize-1;$y++) {
@@ -32,9 +52,9 @@ switch ($Gmode) {
           }
           $ran  = mt_rand(0, $percent);
           if ($ran == 0) {
-              array_push($re[$x][$y], $percent-1, 'n-meng', 1);
+              array_push($re[$x][$y], $percent-1, 'n-meng', 1, $x, $y);
           } else {
-              array_push($re[$x][$y], $percent-1, 'y-meng', 0);
+              array_push($re[$x][$y], '', 'y-meng', 0, $x, $y);
           }
       }
   }
